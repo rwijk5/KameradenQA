22983b817f007f77435df5aa9a5066655cc0a67c
rwijk5 <rwijk5@avans.nl>
2018-02-07 16:53:22 +0100

    Project Setup

---
+++ KameradenQA/DriverFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Edge;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KameradenQA
+{
+    class DriverFactory
+    {
+        IWebDriver driver;
+        public DriverFactory(String browser)
+        {
+            SetDriver(browser);
+        }
+
+        public void SetDriver(string browser)
+        {
+            switch (browser)
+            {
+
+                case "chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "Firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                case "IE":
+                    driver = new InternetExplorerDriver();
+                    break;
+                case "Edge":
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    driver = null;
+                    break;
+            }
+        }
+    }
+}
---
+++ KameradenQA/FirstTestCase.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KameradenQA
+{
+    class FirstTestCase
+    {
+        static IWebDriver driver;
+        static void Main(string[] args)
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://kameraden.nl");
+        }
+
+        public static void Hello()
+        {
+           new System.OutOfMemoryException();
+        }
+    }
+    
+}
