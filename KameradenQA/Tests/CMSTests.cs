2c37e82bb5c0bc25821355e2def677e2253ce53e
rwijk5 <rwijk5@avans.nl>
2018-05-27 21:05:56 +0200

    ApproveUserTest

--- KameradenQA/Tests/CMSTests.cs
+++ KameradenQA/Tests/CMSTests.cs
@@ -25,9 +25,22 @@ namespace KameradenQA.Tests
         }
 
         [Test]
-        public void test()
+        public void ApproveRegisteredUser()
         {
+            //Enter login credentials and press login button
+            driver.Url = Globals.address + "/inloggen";
+            driver.FindElement(By.Name("email")).SendKeys("tim@gmail.com");
+            driver.FindElement(By.Name("password")).SendKeys("test");
+            driver.FindElement(By.Name("login")).Click();
 
+            builder = new Actions(driver);
+            builder.MoveToElement(driver.FindElement(By.ClassName("dropdown"))).Build().Perform();
+            driver.FindElement(By.CssSelector("#nav-list > li:nth-child(7) > div > div.dropdown-content > a:nth-child(2)")).Click();
+            driver.FindElement(By.CssSelector("body > header > a:nth-child(2)")).Click();
+            //driver.FindElement(By.LinkText("Jan van Dijk")).Click();
+            //Je kan niet het juiste element selecteren.
+            driver.Url = "http://kameraden.test:8080/cms/gebruiker/keuren/1";
+            driver.FindElement(By.LinkText("Goedkeuren")).Click();
         }
 
         [TearDown]
