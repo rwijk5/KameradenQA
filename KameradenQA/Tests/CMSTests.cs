acb56128a3db68fc8f3bcde71ba135dd6aff99c9
rwijk5 <rwijk5@avans.nl>
2018-05-27 14:49:27 +0200

    cmstest class toegevoegd

---
+++ KameradenQA/Tests/CMSTests.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace KameradenQA.Tests
+{
+    class CMSTests
+    {
+        static IWebDriver driver;
+        DriverFactory df;
+        Actions builder;
+
+        [SetUp]
+        public void initialize()
+        {
+            df = new DriverFactory("Chrome");
+            driver = df.getDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void test()
+        {
+
+        }
+
+        [TearDown]
+        public void finishTest()
+        {
+            driver.Close();
+        }
+    }
+}
